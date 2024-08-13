68d5904489291d8e464a8fceec44c7e62a26873f
2seb2 <yuo231@naver.com>
2024-08-13 14:49:04 +0900

    feat: boardList 보내주는 기능 구현

--- front/assign1-client/src/boards/BoardList.jsx
+++ front/assign1-client/src/boards/BoardList.jsx
@@ -1,8 +1,51 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from "react";
 import './css/BoardList.css'
+import axios from "axios";
+import Swal from "sweetalert2";
 
 function BoardList() {
     const [boardList, setBoardList] = useState([]);
+
+    let callListApi = async () => {
+        axios.post('/api/board?type=list', {
+
+        }).then(response => {
+            try {
+                console.log(response);
+
+                let result = [];
+                let BoardList = response.data;
+
+                for (let i = 0; i < BoardList.json.length; i++) {
+                    let data = BoardList.json[i];
+                    let date = data.write_date;
+                    let year = date.substr(0, 4);
+                    let month = date.substr(5, 2);
+                    let day = date.substr(8, 2);
+                    let write_date = year + '.' + month + '.' + day;
+
+                    result.push(
+                        <tr key={i}>
+                            <td>{data.article_no}</td>
+                            <td>{data.title}</td>
+                            <td>{data.write_id}</td>
+                            <td>{write_date}</td>
+                        </tr>
+                    );
+                }
+                setBoardList(result);
+            } catch (error) {
+                alert('목록 작업 중 오류');
+            }
+        }).catch(error => {
+            alert('axios 호출 에러');
+            return false;
+        });
+    }
+
+    useEffect(() => {
+        callListApi();
+    }, []);
 
     return (
         <div className='boardList'>BoardList
